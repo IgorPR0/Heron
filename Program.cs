1052686ed78ab65dc3c4e5c1c6ca44e47cf53c0d
IgorPR0 <igortecnologia2020@gmail.com>
2020-09-30 14:06:44 -0300

    Feito atividade Heron

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Heron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        double a, b, c;
+        double perimetro, area;
+
+        Console.Write("Digite o valor de A: ");
+        a = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de B: ");
+        b = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de C: ");
+        c = double.Parse(Console.ReadLine());
+            
+        perimetro = (a + b + c)/2;
+        area = Math.Sqrt(perimetro * (perimetro - a) * (perimetro -b) * (perimetro -c));
+        Console.WriteLine("semiperimetro é igual {0}", perimetro);
+        Console.WriteLine("area é igual {0}", area);
+        
+        }
+    }
+}
